3611559f049997d7f8d17cdfd5886db528d8c20f
Ryan Delaney <vitineth@gmail.com>
2020-06-24 19:03:30 +0100

    Adding pages, SCSS, resources and grids
    Integrating react router and adding in the style demo page. Including flexboxgrid so we have easy access to CSS grids across the entire project. Enabling SCSS for the styles to make nested styles easier. A placeholder UEMS logo has been added to the system

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './pages/index/index.css';
+import './pages/index/index.scss';
+import './pages/index/flexboxgrid.css';
 import App from './pages/App';
 import * as serviceWorker from './worker/serviceWorker';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {StyleDemo} from "./pages/style-demo/StyleDemo";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <div className={"sidebar-real"}>
+                <img src={"/ents-crew-white.png"} className={"header-image"} alt={"UEMS Logo: The text UEMS in a bold geometric font surrounded by a white outlined rectangle."}/>
+            </div>
+
+            <div className={"sidebar-spacer"}>
+            </div>
+
+            <div className={"content"}>
+                <Switch>
+                    <Route path="/style-demo">
+                        <StyleDemo/>
+                    </Route>
+                    <Route path="/" exact>
+                        <App/>
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
