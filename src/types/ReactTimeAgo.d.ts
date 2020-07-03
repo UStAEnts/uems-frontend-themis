59a9edc9db04f09f3f8bd2e6ae4d87fec71c582d
Ryan Delaney <vitineth@gmail.com>
2020-07-03 23:17:11 +0100

    Adding time ago for comments and any component displaying times
    This required custom types because there is no @types package available

---
+++ src/types/ReactTimeAgo.d.ts
@@ -0,0 +1,20 @@
+declare module 'react-time-ago' {
+
+    type ReactTimeAgoProps = {
+        date: Date | number,
+        locale?: string,
+        locales?: string[],
+        timeStyle?: string,
+        tooltip?: boolean,
+        formatVerboseDate?: (date: Date) => string,
+        verboseDateFormat?: object,
+        updateInterval?: number,
+        tick?: boolean,
+        container?: Function,
+    };
+
+    function ReactTimeAgo(props: ReactTimeAgoProps);
+
+    export = ReactTimeAgo;
+
+}
