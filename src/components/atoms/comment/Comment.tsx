2212df010ab1d2245e5fbd2aadcdadcd0960785d
Ryan Delaney <vitineth@gmail.com>
2020-07-03 23:18:40 +0100

    Adding comments component
    This factors in comment types, time since posted, profile pictures. Will need considerations in place for the placeholder image as it is bad practice to use this in production

---
+++ src/components/atoms/comment/Comment.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import Markdown from 'markdown-to-jsx';
+import ReactTimeAgo from 'react-time-ago';
+import ReactTooltip from 'react-tooltip';
+import { Comment as CommentType } from '../../../types/Event';
+import { v4 } from 'uuid';
+
+import './Comment.scss';
+
+export type CommentPropsType = {
+    comment: CommentType,
+}
+
+export function Comment(props: CommentPropsType) {
+    const [id] = React.useState(v4().toString());
+
+    return (
+        <div className="comment">
+            <div className="left">
+                <ReactTooltip
+                    place="top"
+                    type="dark"
+                    effect="float"
+                    id={`cb-${id}`}
+                >
+                    <strong>Category: </strong>
+                    <span>{props.comment.type.name}</span>
+                </ReactTooltip>
+                <div
+                    data-tip
+                    data-for={`cb-${id}`}
+                    className="bar"
+                    style={{
+                        backgroundColor: props.comment.type.color || '#8395a7',
+                    }}
+                />
+                <div className="image">
+                    <img
+                        alt={`Profile for ${props.comment.poster.name}`}
+                        src={props.comment.poster.profile || 'https://placehold.it/200x200'}
+                    />
+                </div>
+            </div>
+            <div className="right">
+                <div className="top">
+                    <Link className="poster" to={`/user/${props.comment.poster.username}`}>
+                        <div className="name">{props.comment.poster.name}</div>
+                        <div className="username">{props.comment.poster.username}</div>
+                    </Link>
+                    <div className="spacer" />
+                    <div className="time">
+                        <ReactTimeAgo locale="en" date={props.comment.posted} />
+                    </div>
+                </div>
+                <div className="bottom content">
+                    <Markdown>{props.comment.content}</Markdown>
+                </div>
+            </div>
+        </div>
+    );
+}
