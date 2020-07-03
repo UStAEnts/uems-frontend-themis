e87aa0951a976a6b4d7b9c986011403ebf5ad8b9
Ryan Delaney <vitineth@gmail.com>
2020-07-03 23:29:34 +0100

    List of comments with compose box
    This will add the comment when you post it but it is not pushed to the backend so will require integration with a comment service when one is created

---
+++ src/components/components/comment-list/CommentList.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Comment as CommentType, User } from '../../../types/Event';
+import { CommentBox } from '../../atoms/comment-box/CommentBox';
+import { Comment } from '../../atoms/comment/Comment';
+
+export type CommentListPropsType = {
+    comments: CommentType[],
+    poster: User
+};
+
+export type CommentListStateType = {
+    comments: CommentType[],
+};
+
+export class CommentList extends React.Component<CommentListPropsType, CommentListStateType> {
+
+    static displayName = 'CommentList';
+
+    constructor(props: Readonly<CommentListPropsType>) {
+        super(props);
+
+        this.state = {
+            comments: [],
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    private handleSubmit(content: string) {
+        this.setState((oldState) => ({
+            comments: oldState.comments.concat({
+                content,
+                type: {
+                    name: '?',
+                },
+                poster: this.props.poster,
+                posted: new Date(),
+            }),
+        }));
+    }
+
+    render() {
+        return (
+            <div className="comment-list">
+                <CommentBox
+                    contentClasses={[]}
+                    poster={this.props.poster}
+                    submitCommentHandler={this.handleSubmit}
+                />
+                {this.props.comments.concat(this.state.comments).sort((a, b) => b.posted.getTime() - a.posted.getTime()).map((e) => (<Comment comment={e} />))}
+            </div>
+        );
+    }
+
+
+}
