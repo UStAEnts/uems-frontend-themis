3b8882fc4edbe8bae1ff70fbb0e3ff5f703f128a
Ryan Delaney <vitineth@gmail.com>
2020-07-03 20:07:44 +0100

    Adding theme colours
    This takes from the CA flat ui colours palette

---
+++ src/theme/Theme.ts
@@ -0,0 +1,51 @@
+export class Theme {
+
+    static readonly PINK_LIGHT: string = '#ff9ff3';
+
+    static readonly PINK: string = '#f368e0';
+
+    static readonly YELLOW: string = '#feca57';
+
+    static readonly GOLD: string = '#ff9f43';
+
+    static readonly RED_LIGHT: string = '#ff6b6b';
+
+    static readonly RED: string = '#ee5253';
+
+    static readonly CYAN: string = '#48dbfb';
+
+    static readonly DEEP_SKY: string = '#0abde3';
+
+    static readonly GREEN_LIGHT: string = '#1dd1a1';
+
+    static readonly GREEN: string = '#10ac84';
+
+    static readonly TEAL_LIGHT: string = '#00d2d3';
+
+    static readonly TEAL: string = '#01a3a4';
+
+    static readonly BLUE_LIGHT: string = '#54a0ff';
+
+    static readonly BLUE: string = '#2e86de';
+
+    static readonly PURPLE_LIGHT: string = '#5f27cd';
+
+    static readonly PURPLE: string = '#341f97';
+
+    static readonly GRAY_LIGHT: string = '#c8d6e5';
+
+    static readonly GRAY: string = '#8395a7';
+
+    static readonly GRAY_DARK: string = '#576574';
+
+    static readonly GRAY_DARKER: string = '#222f3e';
+
+    static readonly SUCCESS: string = Theme.GREEN_LIGHT;
+
+    static readonly FAILURE: string = Theme.RED;
+
+    static readonly WARNING: string = Theme.YELLOW;
+
+    static readonly NOTICE: string = Theme.CYAN;
+
+}
