9bf5c6a5b6304513e30cf8a2b81131ef72b1b1ee
Ryan Delaney <vitineth@gmail.com>
2020-07-10 14:56:40 +0100

    Adding a rendering system for the notifications

---
+++ src/components/components/notification-renderer/NotificationRenderer.tsx
@@ -0,0 +1,111 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import React from "react";
+import { v4 } from "uuid";
+
+import "./NotificationRenderer.scss"
+import { IconBox } from "../../atoms/icon-box/IconBox";
+import { Link } from "react-router-dom";
+
+export type Notification = {
+    id?: string,
+    title: string,
+    content?: string,
+    icon?: IconDefinition,
+    color?: string,
+    state?: string,
+}
+
+export type NotificationRendererPropsType = {
+    position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right',
+    notifications: Notification[],
+}
+
+/**
+ * Applies IDs to notifications that are missing them
+ * @param notifications the array of notifications to process
+ * @param animationStates a set of states to apply to the notification by ID
+ */
+export function processNotifications(notifications: Notification[], animationStates?: {[key: string]: string}) {
+    for (let notification of notifications) {
+        if (notification.id === undefined) notification.id = v4();
+
+        if (animationStates && Object.prototype.hasOwnProperty.call(animationStates, notification.id)){
+            notification.state = animationStates[notification.id];
+        }
+    }
+
+    return notifications;
+}
+
+export class NotificationRenderer extends React.Component<NotificationRendererPropsType, {}> {
+
+    static displayName = "NotificationRenderer";
+
+    /**
+     * Renders a notification object with
+     * @param notification
+     */
+    private makeNotification = (notification: Notification) => {
+        if (notification.icon === undefined) {
+            return (
+                <div className={`notification ${notification.state || ''}`}>
+                    <div className="content">
+                        <div className="rest">
+                            <div className="title">{notification.title}</div>
+                            {
+                                notification.content
+                                    ? <div className="content">{notification.content}</div>
+                                    : undefined
+                            }
+                        </div>
+                        <div
+                            className="accent"
+                            style={{
+                                backgroundColor: notification.color ? notification.color : undefined
+                            }}
+                        />
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className={`notification with-icon ${notification.state || ''}`}>
+                    <div className="content">
+                        <div className="rest">
+                            <div className="left">
+
+                                <div className="title">{notification.title}</div>
+                                {
+                                    notification.content
+                                        ? <div className="content">{notification.content}</div>
+                                        : undefined
+                                }
+                            </div>
+                            <div className="right">
+                                <IconBox icon={notification.icon} color={notification.color || '#000000'} />
+                            </div>
+                        </div>
+                        <div
+                            className="accent"
+                            style={{
+                                backgroundColor: notification.color ? notification.color : undefined
+                            }}
+                        />
+                    </div>
+                    <div className="action">
+                        <Link to={"/"}>Visit...</Link>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div className={`notification-renderer ${this.props.position}`}>
+                {this.props.notifications.map(this.makeNotification)}
+            </div>
+        );
+    }
+
+};
