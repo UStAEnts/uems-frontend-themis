e8fc51c0d43dfb66ea740f9d9bcc5fb278aee360
Ryan Delaney <vitineth@gmail.com>
2020-07-10 14:56:26 +0100

    Adding a notification context type

---
+++ src/context/NotificationContext.ts
@@ -0,0 +1,42 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+export type NotificationContextType = {
+    /**
+     * Show a notification in the global state
+     * @param title the title of the notification
+     * @param content the content body of the notification
+     * @param icon the option font awesome icon to show next to the message
+     * @param color the emphasis color of the notification
+     * @return the ID of the notification which can be used to cancel it
+     */
+    showNotification: (title: string, content?: string, icon?: IconDefinition, color?: string) => string,
+    /**
+     * Clears all notifications currently being displayed. Returns the number of notifications that
+     * were removed from the screen
+     */
+    clearNotifications: () => number,
+    /**
+     * Clears a specific notification from their ID
+     * @param id the id of the notification to remove as returned by {@link NotificationContextType#showNotification}
+     * @return if the notification was removed successfully
+     */
+    clearNotification: (id: string) => boolean,
+}
+
+/**
+ * Default notification context which throws an error on all functions
+ */
+const defaultValue: NotificationContextType = {
+    showNotification: () => {
+        throw new Error('Context is not initialised');
+    },
+    clearNotification: () => {
+        throw new Error('Context is not initialised');
+    },
+    clearNotifications: () => {
+        throw new Error('Context is not initialised');
+    }
+}
+
+export const NotificationContext = React.createContext(defaultValue);
