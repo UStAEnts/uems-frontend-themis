b0acd9c4cd18d5b1c8a5e355eb25b9aae21add5c
Ryan Delaney <vitineth@gmail.com>
2020-07-12 01:17:52 +0100

    Swapping to actual loading

--- src/pages/event/Event.tsx
+++ src/pages/event/Event.tsx
@@ -1,4 +1,4 @@
-import { EntsStatus, EventState, GatewayEvent } from "../../types/Event";
+import { EntsStatus, EventChange, EventState, GatewayEvent, GatewayFile } from "../../types/Event";
 import React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import Loader from "react-loader-spinner";
@@ -13,8 +13,10 @@ import * as url from "url";
 import { CommentList } from "../../components/components/comment-list/CommentList";
 import { FileList } from "../../components/atoms/file-bar/FileBar";
 import ReactTimeAgo from "react-time-ago";
+import urljoin from "url-join";
 import { NotificationContextType } from "../../context/NotificationContext";
 import { withNotificationContext } from "../../components/WithNotificationContext";
+import { faSkullCrossbones } from "@fortawesome/free-solid-svg-icons";
 
 export type EventPropsType = {
     notificationContext?: NotificationContextType,
@@ -25,8 +27,10 @@ export type EventPropsType = {
 export type EventStateType = {
     id?: string,
     event?: GatewayEvent,
+    changelog?: EventChange,
     entsStates?: EntsStatus[],
     buildingStates?: EventState[],
+    files?: GatewayFile[],
     venues?: string[],
 };
 
@@ -43,38 +47,144 @@ class Event extends React.Component<EventPropsType, EventStateType> {
         }
     }
 
+    private failedLoad = (reason: string) => {
+        if (this.props.notificationContext) {
+            try {
+                this.props.notificationContext.showNotification(
+                    'Failed to Load',
+                    `There was an error: ${reason}`,
+                    faSkullCrossbones,
+                    Theme.FAILURE
+                )
+                console.log('Notification shown');
+            } catch (e) {
+                console.error('Notification system failed to send');
+            }
+        }
+    }
+
     componentDidMount() {
-        // Pretend this is done via axios!
-        setTimeout(() => {
+        Axios.get(
+            urljoin(
+                Config.BASE_GATEWAY_URI,
+                'events',
+                encodeURIComponent(this.props.match.params.id),
+            )
+        ).then((data) => {
+            // TODO: add schema validation for data returned by the server
             this.setState((oldState) => ({
                 ...oldState,
-                event: {
-                    _id: oldState.id,
-                    attendance: 300,
-                    bookingEnd: moment().add(7, 'hours').toDate(),
-                    bookingStart: moment().add(1, 'hour').toDate(),
-                    name: 'Some Event',
-                    venue: 'StAge',
-                } as GatewayEvent,
-            }))
+                event: data.data.event,
 
-            setTimeout(() => {
-                this.setState((oldState) => ({
-                    ...oldState,
-                    venues: ['StAge', '601', 'Stage and 601'],
-                    entsStates: [
-                        { name: 'ready' },
-                        { name: 'signup' },
-                        { name: 'awaiting requirements' },
-                    ],
-                    buildingStates: [
-                        { state: 'ready' },
-                        { state: 'approved' },
-                        { state: 'cancelled' },
-                    ]
-                }));
-            }, 1000);
-        }, 500);
+                // Changelog is provided on the /event/{id} endpoint but no where else (not on patch)
+                changelog: data.data.changelog,
+            }));
+        }).catch((err: Error) => {
+            console.error('Failed to load event data');
+            console.error(err);
+
+            this.failedLoad(`Could not load event: ${err.message}`);
+        });
+
+        Axios.get(
+            urljoin(
+                Config.BASE_GATEWAY_URI,
+                'events',
+                encodeURIComponent(this.props.match.params.id),
+                'files',
+            )
+        ).then((data) => {
+            // TODO: add schema validation for data returned by the server
+            this.setState((oldState) => ({
+                ...oldState,
+                files: data.data.files,
+            }));
+        }).catch((err: Error) => {
+            console.error('Failed to load event data');
+            console.error(err);
+
+            this.failedLoad(`Could not load event: ${err.message}`);
+        });
+
+        Axios.get(urljoin(
+            Config.BASE_GATEWAY_URI,
+            'venues',
+        )).then((data) => {
+            // TODO: add schema validation for data returned by the server
+            this.setState((oldState) => ({
+                ...oldState,
+                venues: data.data,
+            }));
+        }).catch((err: Error) => {
+            console.error('Failed to load event data');
+            console.error(err);
+
+            this.failedLoad(`Could not load list of venues: ${err.message}`);
+        });
+
+        Axios.get(urljoin(
+            Config.BASE_GATEWAY_URI,
+            'ents',
+        )).then((data) => {
+            // TODO: add schema validation for data returned by the server
+            this.setState((oldState) => ({
+                ...oldState,
+                entsStates: data.data,
+            }));
+        }).catch((err: Error) => {
+            console.error('Failed to load event data');
+            console.error(err);
+
+            this.failedLoad(`Could not load list of ents states: ${err.message}`);
+        });
+
+        Axios.get(urljoin(
+            Config.BASE_GATEWAY_URI,
+            'states',
+        )).then((data) => {
+            // TODO: add schema validation for data returned by the server
+            this.setState((oldState) => ({
+                ...oldState,
+                buildingStates: data.data,
+            }));
+        }).catch((err: Error) => {
+            console.error('Failed to load event data');
+            console.error(err);
+
+            this.failedLoad(`Could not load list of ents states: ${err.message}`);
+        });
+        //
+        // // Pretend this is done via axios!
+        // setTimeout(() => {
+        //     this.setState((oldState) => ({
+        //         ...oldState,
+        //         event: {
+        //             _id: oldState.id,
+        //             attendance: 300,
+        //             bookingEnd: moment().add(7, 'hours').toDate(),
+        //             bookingStart: moment().add(1, 'hour').toDate(),
+        //             name: 'Some Event',
+        //             venue: 'StAge',
+        //         } as GatewayEvent,
+        //     }))
+        //
+        //     setTimeout(() => {
+        //         this.setState((oldState) => ({
+        //             ...oldState,
+        //             venues: ['StAge', '601', 'Stage and 601'],
+        //             entsStates: [
+        //                 { name: 'ready' },
+        //                 { name: 'signup' },
+        //                 { name: 'awaiting requirements' },
+        //             ],
+        //             buildingStates: [
+        //                 { state: 'ready' },
+        //                 { state: 'approved' },
+        //                 { state: 'cancelled' },
+        //             ]
+        //         }));
+        //     }, 1000);
+        // }, 500);
     }
 
     private patchEvent = (changeProps: Partial<GatewayEvent>) => {
@@ -187,35 +297,39 @@ class Event extends React.Component<EventPropsType, EventStateType> {
                         </div>
                     </div>
                     {/*TODO: add file loading*/}
-                    <FileList files={[
-                        {
-                            private: false,
-                            size: 1000,
-                            filename: 'Some file.txt',
-                            name: 'Rider',
-                            author: {
-                                username: 'ryan',
-                                name: 'Ryan Delaney',
-                            },
-                            created: new Date().getTime(),
-                            downloadURL: '/files/x',
-                            id: 'abcd'
-                        },
-                        {
-                            private: true,
-                            size: 1000,
-                            filename: 'Some file.txt',
-                            name: 'Rider',
-                            author: {
-                                username: 'ryan',
-                                name: 'Ryan Delaney',
-                            },
-                            created: new Date().getTime(),
-                            downloadURL: '/files/x',
-                            id: 'abcd'
-                        },
-                    ]} />
-                    {/*TODO: add comment loading and handling*/}
+                    {
+                        this.state.files
+                            ? (<FileList files={this.state.files} />)
+                            : undefined
+                    }
+                    {/*<FileList files={[*/}
+                    {/*    {*/}
+                    {/*        private: false,*/}
+                    {/*        size: 1000,*/}
+                    {/*        filename: 'Some file.txt',*/}
+                    {/*        name: 'Rider',*/}
+                    {/*        author: {*/}
+                    {/*            username: 'ryan',*/}
+                    {/*            name: 'Ryan Delaney',*/}
+                    {/*        },*/}
+                    {/*        created: new Date().getTime(),*/}
+                    {/*        downloadURL: '/files/x',*/}
+                    {/*        id: 'abcd'*/}
+                    {/*    },*/}
+                    {/*    {*/}
+                    {/*        private: true,*/}
+                    {/*        size: 1000,*/}
+                    {/*        filename: 'Some file.txt',*/}
+                    {/*        name: 'Rider',*/}
+                    {/*        author: {*/}
+                    {/*            username: 'ryan',*/}
+                    {/*            name: 'Ryan Delaney',*/}
+                    {/*        },*/}
+                    {/*        created: new Date().getTime(),*/}
+                    {/*        downloadURL: '/files/x',*/}
+                    {/*        id: 'abcd'*/}
+                    {/*    },*/}
+                    {/*]} />*/}
                     <CommentList comments={[]} />
                 </div>
                 <div className="rightbar-real">
