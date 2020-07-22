c76d1e52cb29d17fa4161bce8fc831a3fb179586
Ryan Delaney <vitineth@gmail.com>
2020-07-22 13:09:36 +0100

    Beginning work on event creation pages

---
+++ src/pages/event/create/CreateEvent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export type CreateEventPropsType = {};
+
+export type CreateEventStateType = {};
+
+export class CreateEvent extends React.Component<CreateEventPropsType, CreateEventStateType> {
+
+    static displayName = 'Create Event';
+
+    render() {
+        return undefined;
+    }
+
+}
