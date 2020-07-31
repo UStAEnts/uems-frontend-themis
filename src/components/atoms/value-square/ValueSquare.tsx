f3b8a0cffb2db735e988a4da3a9ada6357977e20
Ryan Delaney <vitineth@gmail.com>
2020-07-31 21:24:04 +0100

    Adding value squares
    To be used in the view pages for events counts etc

---
+++ src/components/atoms/value-square/ValueSquare.tsx
@@ -0,0 +1,126 @@
+import React, { CSSProperties } from 'react';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { EditableProperty } from '../../components/editable-property/EditableProperty';
+
+import './ValueSquare.scss';
+import { Theme } from "../../../theme/Theme";
+import { Link, withRouter } from 'react-router-dom';
+import { withNotificationContext } from "../../WithNotificationContext";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+
+export type ValueSquarePropsType = {
+    value: string | number,
+    icon?: IconDefinition,
+    name?: string,
+    editable?: boolean,
+    onChange?: (value: string | number) => void,
+    link?: string,
+    style?: CSSProperties,
+    className?: string,
+};
+
+export type ValueSquareStateType = {};
+
+class ValueSquareClass extends React.Component<ValueSquarePropsType, ValueSquareStateType> {
+
+    static displayName = 'ValueSquare';
+
+    private generateHeader = () => {
+        if (this.props.icon && this.props.name) {
+            return (
+                <div className="icon" style={{ background: Theme.NOTICE }}>
+                    <FontAwesomeIcon icon={this.props.icon} size="lg" />
+                    <div className="name">{this.props.name}</div>
+                </div>
+            );
+        }
+
+        if (this.props.icon && !this.props.name) {
+            return (
+                <div className="icon no-name" style={{ background: Theme.NOTICE }}>
+                    <FontAwesomeIcon icon={this.props.icon} size="lg" />
+                </div>
+            );
+        }
+
+        if (!this.props.icon && this.props.name) {
+            return (
+                <div className="icon only-name" style={{ background: Theme.NOTICE }}>
+                    <div className="name">{this.props.name}</div>
+                </div>
+            );
+        }
+
+        return undefined;
+    }
+
+    render() {
+        let value;
+
+        if (this.props.editable) {
+            if (typeof (this.props.value) === 'string') {
+                value = (
+                    <EditableProperty
+                        name="value"
+                        config={{
+                            type: 'text',
+                            value: this.props.value,
+                            onChange: this.props.onChange,
+                        }}
+                    >
+                        {this.props.value}
+                    </EditableProperty>
+                );
+            } else {
+                value = (
+                    <EditableProperty
+                        name="value"
+                        config={{
+                            type: 'text',
+                            value: this.props.value,
+                            onChange: this.props.onChange,
+                            fieldType: 'number',
+                        }}
+                    >
+                        {this.props.value}
+                    </EditableProperty>
+                );
+            }
+        } else {
+            value = this.props.value;
+        }
+
+        const content = (
+            <div
+                className={`value-square ${this.props.className ?? ''}`}
+                style={{ background: Theme.NOTICE, ...this.props.style }}
+            >
+                {this.generateHeader()}
+                <div className="conts" >
+                    {value}
+                </div>
+                {
+                    this.props.link
+                        ? (
+                            <Link to={this.props.link}>
+                                <FontAwesomeIcon className="link" icon={faExternalLinkAlt} />
+                            </Link>
+                        )
+                        : undefined
+                }
+            </div>
+        );
+
+        return this.props.link
+            ? (
+                <Link to={this.props.link}>
+                    {content}
+                </Link>
+            )
+            : content;
+    }
+
+}
+
+export const ValueSquare = withRouter(withNotificationContext(ValueSquareClass));
