f621059a34fe1697c6d27dd973ab476176d4bc83
Ryan Delaney <vitineth@gmail.com>
2020-08-01 17:26:22 +0100

    Adding venue main view

---
+++ src/pages/venues/view/ViewVenue.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { API, EventResponse, VenueResponse, VenueUpdate } from '../../../utilities/APIGen';
+import { failEarlyStateSet } from '../../../utilities/AccessUtilities';
+import { EventOrCommentRelatedView } from '../../../components/components/event-related-view/EventOrCommentRelatedView';
+
+export type ViewVenuePropsType = {} & RouteComponentProps<{
+    id: string,
+}>;
+
+export type ViewVenueStateType = {
+    venue?: VenueResponse,
+    events?: EventResponse[],
+};
+
+class ViewVenueClass extends React.Component<ViewVenuePropsType, ViewVenueStateType> {
+
+    static displayName = 'ViewVenue';
+
+    constructor(props: Readonly<ViewVenuePropsType>) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        API.venues.id.get(this.props.match.params.id).then((data) => {
+            failEarlyStateSet(
+                this.state,
+                this.setState.bind(this),
+                'venue',
+            )(data.result);
+        }).catch((err) => {
+            console.error(err);
+            // TODO: error handling w/ notifs
+        });
+        API.venues.id.events.get(this.props.match.params.id).then((data) => {
+            failEarlyStateSet(
+                this.state,
+                this.setState.bind(this),
+                'events',
+            )(data.result);
+        }).catch((err) => {
+            console.error(err);
+            // TODO: error handling w/ notifs
+        });
+    }
+
+    //
+    private patch = (update: VenueUpdate) => {
+        console.log(update);
+        API.venues.id.patch(this.props.match.params.id, update).then(() => {
+            // To fix some typing
+            if (!this.state.venue) return;
+
+            const newVenue: VenueResponse = {
+                ...this.state.venue,
+                ...update,
+            };
+
+            failEarlyStateSet(
+                this.state,
+                this.setState.bind(this),
+                'venue',
+            )(newVenue);
+        }).catch((err) => {
+            console.error(err);
+            // TODO: error handling w/ notifs
+        });
+    }
+
+    render() {
+        if (this.state.venue) {
+            return (
+                <EventOrCommentRelatedView
+                    obj={this.state.venue}
+                    patch={(changes: VenueUpdate) => {
+                        this.patch(changes);
+                    }}
+                    configOverrides={[
+                        {
+                            property: 'color',
+                            type: 'color',
+                        },
+                    ]}
+                    excluded={[
+                        'id',
+                        'user',
+                        'date',
+                    ]}
+                    events={this.state.events}
+                />
+            );
+        }
+        return null;
+    }
+}
+
+export const ViewVenue = withRouter(ViewVenueClass);
