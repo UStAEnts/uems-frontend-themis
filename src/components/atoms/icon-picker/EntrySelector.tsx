070a400f46705cc281106bd1834e5833c40cc798
Ryan Delaney <vitineth@gmail.com>
2020-08-02 16:11:33 +0100

    Extracting out icon picker class

--- src/components/atoms/icon-picker/EntrySelector.tsx
+++ src/components/atoms/icon-picker/EntrySelector.tsx
@@ -1,11 +1,9 @@
 import React, { CSSProperties, RefObject } from 'react';
-import './EntrySelector.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as Icon from '@fortawesome/free-solid-svg-icons';
 import { TextField } from '../text-field/TextField';
 import { failEarlyStateSet } from '../../../utilities/AccessUtilities';
 import { Theme } from '../../../theme/Theme';
 import InputUtilities from '../../../utilities/InputUtilities';
+import './EntrySelector.scss';
 
 export type OptionType = {
     name: string,
@@ -154,61 +152,4 @@ export class EntrySelector extends React.Component<EntrySelectorPropsType, Entry
         );
     }
 
-}
-
-export class IconSelector extends React.Component<Omit<EntrySelectorPropsType, 'options' | 'render'>, {}> {
-
-    private readonly options: OptionType[] = [];
-
-    constructor(props: Readonly<Omit<EntrySelectorPropsType, 'options' | 'render'>>) {
-        super(props);
-
-        const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
-        const icons = Object.keys(Icon).filter(
-            (name) => !(name.toLowerCase() === 'prefix'
-                || name.toLowerCase() === 'fastopwatch20'
-                || name.toLowerCase() === 'fas'),
-        ).filter((entry) => entry in Icon);
-
-        for (const icon of icons) {
-            let identifierOutput = '';
-            const rawName = String(icon);
-
-            for (let i = 2; i < rawName.length; i++) {
-                // it is upper case
-                if (upper.includes(rawName.charAt(i))) {
-                    if (i === 2) {
-                        identifierOutput += rawName.charAt(i).toLowerCase();
-                    } else {
-                        identifierOutput += `-${rawName.charAt(i).toLowerCase()}`;
-                    }
-                } else {
-                    identifierOutput += rawName.charAt(i);
-                }
-            }
-
-            this.options.push({
-                name: rawName,
-                identifier: identifierOutput,
-            });
-        }
-    }
-
-    private renderIcon = (identifier: string) => (
-        <FontAwesomeIcon icon={['fas', identifier] as unknown as Icon.IconDefinition} />
-    )
-
-    render() {
-        return (
-            <EntrySelector
-                options={this.options}
-                render={this.renderIcon}
-                onSelect={this.props.onSelect}
-                value={this.props.value}
-                displayType={this.props.displayType}
-                searchable={this.props.searchable}
-            />
-        );
-    }
 }
---
+++ src/components/atoms/icon-picker/IconPicker.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import * as Icon from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { EntrySelector, EntrySelectorPropsType, OptionType } from './EntrySelector';
+
+export class IconSelector extends React.Component<Omit<EntrySelectorPropsType, 'options' | 'render'>, {}> {
+
+    private readonly options: OptionType[] = [];
+
+    constructor(props: Readonly<Omit<EntrySelectorPropsType, 'options' | 'render'>>) {
+        super(props);
+
+        const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+        const icons = Object.keys(Icon).filter(
+            (name) => !(name.toLowerCase() === 'prefix'
+                || name.toLowerCase() === 'fastopwatch20'
+                || name.toLowerCase() === 'fas'),
+        ).filter((entry) => entry in Icon);
+
+        for (const icon of icons) {
+            let identifierOutput = '';
+            const rawName = String(icon);
+
+            for (let i = 2; i < rawName.length; i++) {
+                // it is upper case
+                if (upper.includes(rawName.charAt(i))) {
+                    if (i === 2) {
+                        identifierOutput += rawName.charAt(i).toLowerCase();
+                    } else {
+                        identifierOutput += `-${rawName.charAt(i).toLowerCase()}`;
+                    }
+                } else {
+                    identifierOutput += rawName.charAt(i);
+                }
+            }
+
+            this.options.push({
+                name: rawName,
+                identifier: identifierOutput,
+            });
+        }
+    }
+
+    private renderIcon = (identifier: string) => (
+        <FontAwesomeIcon icon={['fas', identifier] as unknown as Icon.IconDefinition} />
+    )
+
+    render() {
+        return (
+            <EntrySelector
+                options={this.options}
+                render={this.renderIcon}
+                onSelect={this.props.onSelect}
+                value={this.props.value}
+                displayType={this.props.displayType}
+                searchable={this.props.searchable}
+            />
+        );
+    }
+}
