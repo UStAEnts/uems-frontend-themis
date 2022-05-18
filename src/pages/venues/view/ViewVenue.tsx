428b29f6474e0841b2f49f0c10b1a235e3c41995
Ryan Delaney <vitineth@gmail.com>
2022-05-18 15:55:54 +0100

    Migrating to the new venue viewer

--- src/pages/venues/view/ViewVenue.tsx
+++ src/pages/venues/view/ViewVenue.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { API, EventResponse, VenueResponse, VenueUpdate } from '../../../utilities/APIGen';
-import { failEarlyStateSet } from '../../../utilities/AccessUtilities';
-import { EventOrCommentRelatedView } from '../../../components/components/event-related-view/EventOrCommentRelatedView';
-import { loadAPIData } from '../../../utilities/DataUtilities';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {API, EventResponse, VenueResponse} from '../../../utilities/APIGen';
+import {loadAPIData} from '../../../utilities/DataUtilities';
 import {
     FallibleReactComponent,
     FallibleReactStateType,
@@ -11,6 +9,7 @@ import {
 import {UIUtilities} from "../../../utilities/UIUtilities";
 import {NotificationPropsType} from "../../../context/NotificationContext";
 import {withNotificationContext} from "../../../components/WithNotificationContext";
+import Venue from "./Venue";
 
 export type ViewVenuePropsType = {} & RouteComponentProps<{
     id: string,
@@ -49,55 +48,9 @@ class ViewVenueClass extends FallibleReactComponent<ViewVenuePropsType, ViewVenu
         );
     }
 
-    //
-    private patch = (update: VenueUpdate) => {
-        console.log(update);
-        API.venues.id.patch(this.props.match.params.id, update).then(() => {
-            // To fix some typing
-            if (!this.state.venue) return;
-
-            const newVenue: VenueResponse = {
-                ...this.state.venue,
-                ...update,
-            };
-
-            failEarlyStateSet(
-                this.state,
-                this.setState.bind(this),
-                'venue',
-            )(newVenue);
-        }).catch((err) => {
-            console.error(err);
-            // TODO: error handling w/ notifs
-        });
-    }
-
     realRender() {
         if (this.state.venue) {
-            return (
-                <EventOrCommentRelatedView
-                    obj={this.state.venue}
-                    patch={(changes: VenueUpdate) => {
-                        this.patch(changes);
-                    }}
-                    configOverrides={[
-                        {
-                            property: 'color',
-                            type: 'color',
-                        },
-                    ]}
-                    excluded={[
-                        'id',
-                        'user',
-                        'date',
-                    ]}
-                    events={this.state.events}
-                    delete={{
-                        redirect: '/venues',
-                        onDelete: () => UIUtilities.deleteWith409Support(()=> API.venues.id.delete(this.props.match.params.id)),
-                    }}
-                />
-            );
+            return (<Venue venue={this.state.venue} events={this.state.events}/>);
         }
         return null;
     }
